d952aaae9b7f570f1022c00f95cb0f571d7c8bc6
Jan Sorgalla <jan@sorgalla.com>
2014-02-12 11:34:18 +0100

    Fix lineString spelling

--- src/Factory.php
+++ src/Factory.php
@@ -6,9 +6,9 @@ interface Factory
 {
     public function createPoint($dimension, array $coordinates, $srid = null);
     public function createLineString($dimension, array $points, $srid = null);
-    public function createPolygon($dimension, array $linestrings, $srid = null);
+    public function createPolygon($dimension, array $lineStrings, $srid = null);
     public function createMultiPoint($dimension, array $points, $srid = null);
-    public function createMultiLineString($dimension, array $linestrings, $srid = null);
+    public function createMultiLineString($dimension, array $lineStrings, $srid = null);
     public function createMultiPolygon($dimension, array $polygons, $srid = null);
     public function createGeometryCollection($dimension, array $geometries, $srid = null);
 }
