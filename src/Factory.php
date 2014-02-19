7d31177b0d81843856770303497bd5d511aff771
Jan Sorgalla <jan@sorgalla.com>
2014-02-19 14:06:30 +0100

    Add createLinearRing method

--- src/Factory.php
+++ src/Factory.php
@@ -6,6 +6,7 @@ interface Factory
 {
     public function createPoint($dimension, array $coordinates, $srid = null);
     public function createLineString($dimension, array $points, $srid = null);
+    public function createLinearRing($dimension, array $points, $srid = null);
     public function createPolygon($dimension, array $lineStrings, $srid = null);
     public function createMultiPoint($dimension, array $points, $srid = null);
     public function createMultiLineString($dimension, array $lineStrings, $srid = null);
